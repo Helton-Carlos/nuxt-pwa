c5b3c085705bafb11ab3a647dbd1e2b5b6054b96
Helton Brito <heltoncarlospa@hotmail.com>
2023-10-17 20:53:07 -0300

    feat: add PWA in project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,43 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: [
+    '@vite-pwa/nuxt'
+  ],
+  pwa: {
+    manifest: {
+      name: "Nuxt3 PWA",
+      short_name: "Nuxt3 PWA",
+      description: "Testing Nuxt3 PWA",
+      icons: [
+        {
+          src: "icons/favicon-32x32.png",
+          sizes: "32x32",
+          type: "image/png",
+        },
+        {
+          src: "icons/android-chrome-140x140.png",
+          sizes: "140x140",
+          type: "image/png",
+        },
+        {
+          src: "icons/android-chrome-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "icons/android-chrome-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+      ],
+    },
+    workbox: {
+      navigateFallback: "/",
+    },
+    devOptions: {
+      enabled: true,
+      type: "module",
+    },
+  },
+});
