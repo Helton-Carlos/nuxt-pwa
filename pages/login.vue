c21b7e127f6a6477d25d7f31aadbdef95dae9d87
Helton Brito <heltoncarlospa@hotmail.com>
2023-11-05 21:49:50 -0300

    feat: web SQL PWA

--- pages/login.vue
+++ pages/login.vue
@@ -4,17 +4,46 @@ import { useVibrate } from '@vueuse/core';
 const { vibrate, stop, isSupported } = useVibrate({ pattern: [300, 100, 300] });
 
 const user = ref({
-  username: '',
+  name: '',
   password: '',
+  email: '',
 });
 
-const login = () => {
-  user.value.username && user.value.password ? alert('true') : vibrate();
+function login() {
+  const { name, password, email } = user.value;
+  if (name && password && email) {
+    const user = [{ name, password, email }];
+
+    const db = window.openDatabase(
+      'users-2',
+      '1.0',
+      'users-2',
+      1 * 1024 * 1024,
+    );
+
+    db.transaction(
+      (transaction) => {
+        transaction.executeSql(
+          'CREATE TABLE user (name TEXT,email TEXT, password TEXT)',
+        );
+        for (let g of user) {
+          transaction.executeSql(
+            'INSERT INTO user (name,email, password) VALUES (?, ?, ?)',
+            [g.name, g.email, g.password],
+          );
+        }
+      },
+      (erro) => {
+        vibrate();
+        console.error(erro);
+      },
+    );
+  }
 
   setTimeout(() => {
     stop();
   }, 2000);
-};
+}
 </script>
 
 <template>
@@ -25,11 +54,21 @@ const login = () => {
     <div class="flex flex-col">
       <label for="uname"><b>Username</b></label>
       <input
-        v-model="user.username"
+        v-model="user.name"
         type="text"
         class="m-2 bg-white border-none p-2"
         placeholder="Enter Username"
         name="uname"
+        required
+      />
+
+      <label for="email"><b>E-mail</b></label>
+      <input
+        v-model="user.email"
+        type="email"
+        class="m-2 bg-white border-none p-2"
+        placeholder="Enter Username"
+        name="email"
         required
       />
 
